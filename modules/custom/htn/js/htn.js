828ea1480454311753689c53f8213322c94968be
ThaiDeveloperSenior <ThaiDeveloperSenior>
2016-09-22 14:30:30 +0700

    first commit

---
+++ modules/custom/htn/js/htn.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function ($, Drupal, window, document) {
+
+  'use strict';
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.services = {
+    attach: function (context, settings) {
+      
+    }
+  };  
+})(jQuery, Drupal, this, this.document);
