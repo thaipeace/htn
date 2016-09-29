24c046e130ccc9030625519ae75db16ffdefccc4
ThaiDeveloperSenior <ThaiDeveloperSenior>
2016-09-29 11:17:16 +0700

    Thai-0929

---
+++ themes/STARTERKIT/templates/html.tpl.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for the basic html structure of a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728208
+ */
+?><!DOCTYPE html>
+<html <?php print $html_attributes . $rdf_namespaces; ?>>
+<head>
+  
+<!--   You can use Open Graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+<!--	<meta property="og:url"           content="http://demo.april.com.vn/htn/thai-1474449168" />
+	<meta property="og:type"          content="website" />
+	<meta property="og:title"         content="HTN" />
+	<meta property="og:description"   content="Fun video" />
+	<meta property="og:image"         content="http://demo.april.com.vn/htn/sites/default/files/model/male.png" />-->
+  
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+
+  <?php if ($default_mobile_metatags): ?>
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width">
+  <?php endif; ?>
+
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <?php if ($add_html5_shim): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5shiv.min.js"></script>
+    <![endif]-->
+  <?php endif; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <!-- Load Facebook SDK for JavaScript -->
+	<div id="fb-root"></div>
+	<div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1799552733599704";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+  
+  <?php if ($skip_link_text && $skip_link_anchor): ?>
+    <p class="skip-link__wrapper">
+      <a href="#<?php print $skip_link_anchor; ?>" class="skip-link visually-hidden visually-hidden--focusable" id="skip-link"><?php print $skip_link_text; ?></a>
+    </p>
+  <?php endif; ?>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
