481b52d0ce018e42149edc47c64fe6ce18b30c93
ThaiDeveloperSenior <ThaiDeveloperSenior>
2016-09-30 10:19:13 +0700

    0930

---
+++ modules/custom/htn/js/story.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function ($, Drupal, window, document) {
+
+  'use strict';
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.gaming = {
+    attach: function (context, settings) {
+      
+      if ($('#story-node-form').length > 0) {
+
+        // Fill from fake case
+//        $("input[name=field_case1]:radio").change(function () {
+//          $('#edit-field-case1-selected-und-0-value').val($(this).val());
+//        });
+//        $("input[name=field_case2]:radio").change(function () {
+//          $('#edit-field-case2-selected-und-0-value').val($(this).val());
+//        });
+//        $("input[name=field_case3]:radio").change(function () {
+//          $('#edit-field-case3-selected-und-0-value').val($(this).val());
+//        });
+    
+      }
+      
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
