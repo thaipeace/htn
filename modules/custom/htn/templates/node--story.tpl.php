bc60ef9d924edbbd4828714acb576c5098fbdfa8
ThaiDeveloperSenior <ThaiDeveloperSenior>
2016-10-06 15:58:58 +0700

    1006

--- modules/custom/htn/templates/node--story.tpl.php
+++ modules/custom/htn/templates/node--story.tpl.php
@@ -31,24 +31,7 @@
       <?php endif; ?>
     </header>
   <?php endif; ?>
-  
-  <div class="thanks">
-    <div class="intro">
-      <div class="title">Cảm ơn <?php print($field_name['und'][0]['value']) ?>,<br/>đã gửi câu chuyện hài hước<br/>của mình về cho ban tổ chức</div>
-      <div class="desc">Nội dung sẽ được duyệt trong 12h, sau đó câu chuyện sẽ được đăng lên Thư viện ảnh cuộc thi trên website của chương trình.</div>
-      <div class="foot">Trà thảo mộc Dr Thanh, <br/>thanh lọc bên trong, tươi trẻ bên ngoài</div>
-    </div>
-  </div>
-  
-  <div class="fconnect">
-    <?php if (!user_is_logged_in()) {?>
-      <a href="user/simple-fb-connect?url=node/<?php print($nid) ?>">Đăng nhập bằng Facebook</a>
-    <?php }else { ?>
-      <div id="fb-share-button">Chia sẻ</div>
-    <?php } ?>
-  </div>
-  <div class="backhome">Trang chủ</div>
-  
+
   <?php
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
